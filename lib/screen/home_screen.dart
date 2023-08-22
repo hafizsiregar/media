52212d61a4dd7b0aa59930a56f100bafe2e9eeb5
hafizsiregar <hafizmessi81@gmail.com>
2023-08-22 10:47:58 +0700

    pick image from camera & gallery

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,126 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/home_provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Camera Project"),
+        actions: [
+          IconButton(
+            onPressed: () => _onUpload(),
+            icon: const Icon(Icons.upload),
+            tooltip: "Unggah",
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: context.watch<HomeProvider>().imagePath == null
+                  ? const Align(
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.image,
+                        size: 100,
+                      ),
+                    )
+                  : _showImage(),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _onGalleryView(),
+                    child: const Text("Gallery"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _onCameraView(),
+                    child: const Text("Camera"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _onCustomCameraView(),
+                    child: const Text("Custom Camera"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onUpload() async {}
+
+  _onGalleryView() async {
+    final provider = context.read<HomeProvider>();
+
+    // variable kedua ini untuk menghandle agar tidak bisa berjalan ketika di ios dan windows
+    // karna image picker belum support dalam ios dan windows
+    final isMacOS = defaultTargetPlatform == TargetPlatform.macOS;
+    final isLinux = defaultTargetPlatform == TargetPlatform.linux;
+    if (isMacOS || isLinux) return;
+
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  _onCameraView() async {
+    final provider = context.read<HomeProvider>();
+
+    final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+    final isiOS = defaultTargetPlatform == TargetPlatform.iOS;
+    final isNotMobile = !(isAndroid || isiOS);
+    if (isNotMobile) return;
+
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.camera,
+    );
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  _onCustomCameraView() async {}
+
+  Widget _showImage() {
+    final imagePath = context.read<HomeProvider>().imagePath;
+    return kIsWeb
+        ? Image.network(
+            imagePath.toString(),
+            fit: BoxFit.contain,
+          )
+        : Image.file(
+            File(imagePath.toString()),
+            fit: BoxFit.contain,
+          );
+  }
+}
