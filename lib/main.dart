34675cb53ed5107fd83d8fb52830e69ffa49d87e
hafizsiregar <hafizmessi81@gmail.com>
2023-08-23 20:39:12 +0700

    upload file to server

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:take_image/api/api_service.dart';
+import 'package:take_image/provider/upload_provider.dart';
 import 'provider/home_provider.dart';
 import 'screen/home_screen.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => HomeProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HomeProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => UploadProvider(
+            ApiService(),
+          ),
+        ),
+      ],
       child: const MaterialApp(
         home: HomePage(),
       ),
